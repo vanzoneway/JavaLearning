7c12bf188c5cb8f0126a249cdd889ddc132dabc4
vanzoneway <vanz.evergarden0@gmail.com>
2022-09-14 10:50:25 +0300

    first commit

---
+++ src/Creating.java
@@ -0,0 +1,6 @@
+
+public class Creating {
+	public static void main (String[] args) {
+	System.out.println("I did something new");
+	}
+}
---
+++ src/Testing.java
@@ -0,0 +1,8 @@
+
+public class Testing {
+	public static void main(String[] args) {
+		System.out.println("Hello,world");
+		System.out.println("Byebye");
+		System.out.println("Switched on useless_mark");
+	}
+}
